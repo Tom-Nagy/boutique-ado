38dcd344d608700bbe667f7a7a3cfc9828f4678a
Tom-Nagy <tomnagy@hotmail.fr>
2021-09-28 17:18:57 +0000

    Finish product size logic

--- bag/views.py
+++ bag/views.py
@@ -12,17 +12,29 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # Get the http session named bag if it exists
     # and if not create it and set it to an empty dic
     bag = request.session.get('bag', {})
 
-    # Check if the product exists and if so add the quantity selected to it
-    # and if not create a key/val pair of item_id/qty,
-    # reassign the value to the session variable
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # Check if the product exists and if so add the quantity selected to it
+        # and if not create a key/val pair of item_id/qty,
+        # reassign the value to the session variable
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
