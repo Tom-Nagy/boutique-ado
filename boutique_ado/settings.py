9ef525aded5871ca98ebf26247f0a9ae399d8471
Tom-Nagy <tomnagy@hotmail.fr>
2021-09-30 14:18:25 +0000

    Add toast

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     # default to django because allauth doest do that
